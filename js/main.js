07a7d3b389b93d29704927cd89625371f70e2e34
Vladimir Legotin <wovanza@gmail.com>
2020-02-02 10:48:46 +0500

    Отрефакторили код по комментам

--- js/main.js
+++ js/main.js
@@ -13,8 +13,8 @@ var TITLES = [
 var MIN_PRICE = 1500;
 var MAX_PRICE = 10000;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var ROOMS = [1, 2, 3, 4];
-var GUESTS = [1, 2, 3, 4, 5];
+var MAX_ROOMS = 4;
+var MAX_GUESTS = 5;
 var TIMES = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var PHOTOS = [
@@ -24,7 +24,7 @@ var PHOTOS = [
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 var X_MIN = 0;
-var X_MAX = document.body.clientWidth;
+var xMax = document.body.clientWidth;
 var Y_MIN = 130;
 var Y_MAX = 630;
 var DESCRIPTION = 'Типичное описание типичного объявления типичного вида';
@@ -37,25 +37,27 @@ function getRandomNumber(min, max) {
 
 function getRandomArray(arr) {
   var newArray = [];
-  var newArrayLength = getRandomNumber(1, arr.length - 1);
+  var newArrayLength = getRandomNumber(1, arr.length + 1);
   for (var i = 0; i < newArrayLength; i++) {
     newArray.push(arr[i]);
   }
-  return newArray[getRandomNumber(0, newArray.length)];
+  return newArray;
 }
 
-var getRandomObject = function (objectNumber) {
+function getRandomObject(objectNumber) {
+  var locationX = getRandomNumber(X_MIN, xMax);
+  var locationY = getRandomNumber(Y_MIN, Y_MAX);
   return {
     author: {
       avatar: 'img/avatars/user0' + (objectNumber + 1) + '.png'
     },
     offer: {
       title: TITLES[objectNumber],
-      address: '600, 350',
+      address: 'locationX, locationY',
       price: getRandomNumber(MIN_PRICE, MAX_PRICE),
       type: TYPES[getRandomNumber(0, TYPES.length)],
-      rooms: ROOMS[getRandomNumber(0, ROOMS.length)],
-      guests: GUESTS[getRandomNumber(0, GUESTS.length)],
+      rooms: getRandomNumber(0, MAX_ROOMS),
+      guests: getRandomNumber(0, MAX_GUESTS),
       checkin: TIMES[getRandomNumber(0, TIMES.length)],
       checkout: TIMES[getRandomNumber(0, TIMES.length)],
       features: getRandomArray(FEATURES),
@@ -63,19 +65,19 @@ var getRandomObject = function (objectNumber) {
       photos: getRandomArray(PHOTOS)
     },
     location: {
-      x: getRandomNumber(X_MIN + PIN_WIDTH, X_MAX - PIN_WIDTH),
-      y: getRandomNumber(Y_MIN + PIN_HEIGHT, Y_MAX - PIN_HEIGHT)
+      x: locationX,
+      y: locationY
     }
   };
-};
+}
 
-var getObjectsArray = function () {
+function getObjectsArray() {
   var offers = [];
   for (var i = 0; i < ADS_QUANTITY; i++) {
     offers.push(getRandomObject(i));
   }
   return offers;
-};
+}
 
 var newOffer = getObjectsArray(ADS_QUANTITY);
 
@@ -83,20 +85,20 @@ document.querySelector('.map').classList.remove('map--faded');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 var pinListElement = document.querySelector('.map__pins');
 
-var renderPin = function (offers) {
+function renderPin(offers) {
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < offers.length; i++) {
     var pinElement = pinTemplate.cloneNode(true);
-
-    pinElement.style.left = offers[i].location.x + 'px';
-    pinElement.style.top = offers[i].location.y + 'px';
-    pinElement.querySelector('img').src = offers[i].author.avatar;
-    pinElement.querySelector('img').alt = offers[i].offer.title;
+    var offer = offers[i];
+    pinElement.style.left = (offer.location.x - PIN_WIDTH / 2) + 'px';
+    pinElement.style.top = (offer.location.y - PIN_HEIGHT) + 'px';
+    pinElement.querySelector('img').src = offer.author.avatar;
+    pinElement.querySelector('img').alt = offer.offer.title;
 
     fragment.appendChild(pinElement);
   }
   pinListElement.appendChild(fragment);
-};
+}
 
 renderPin(newOffer);
