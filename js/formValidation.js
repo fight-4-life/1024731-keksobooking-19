ac36bff5a6ad985d384b9b8f83542c47ac1ba05c
Vladimir Legotin <wovanza@gmail.com>
2020-03-01 21:49:57 +0500

    Добавляет валидацию полей: фото, тип жилья \ цена, заголовок объявления, время заезда\выезда

--- js/formValidation.js
+++ js/formValidation.js
@@ -1,6 +1,14 @@
 'use strict';
 
 (function () {
+
+  var HousingTypes = {
+    BUNGALO: 'bungalo',
+    FLAT: 'flat',
+    HOUSE: 'house',
+    PALACE: 'palace'
+  };
+
   var adForm = document.querySelector('.ad-form');
   var capacitySelect = adForm.querySelector('#capacity');
   var roomsSelect = adForm.querySelector('#room_number');
@@ -20,7 +28,56 @@
     }
   }
 
+  var typeSelect = adForm.querySelector('#type');
+  var pricePerNight = adForm.querySelector('#price');
+
+  function housngTypeToMinPriceValidation() {
+    var type = typeSelect.value;
+    if (type === HousingTypes.BUNGALO) {
+      pricePerNight.setAttribute('min', '0');
+      pricePerNight.setAttribute('placeholder', '0');
+    } else if (type === HousingTypes.FLAT) {
+      pricePerNight.setAttribute('min', '1000');
+      pricePerNight.setAttribute('placeholder', '1000');
+    } else if (type === HousingTypes.HOUSE) {
+      pricePerNight.setAttribute('min', '5000');
+      pricePerNight.setAttribute('placeholder', '5000');
+    } else if (type === HousingTypes.PALACE) {
+      pricePerNight.setAttribute('min', '10000');
+      pricePerNight.setAttribute('placeholder', '10000');
+    }
+
+    typeSelect.addEventListener('change', housngTypeToMinPriceValidation);
+  }
+
+  var timeInSelect = adForm.querySelector('#timein');
+  var timeOutSelect = adForm.querySelector('#timeout');
+
+  function getTimeSync() {
+    var timeInOptions = timeInSelect.querySelectorAll('option');
+    var timeOutOptions = timeOutSelect.querySelectorAll('option');
+
+    function onChangeTimeIn() {
+      timeInOptions.forEach(function (item, index) {
+        if (item.selected) {
+          timeOutOptions[index].selected = true;
+        }
+      });
+    }
+    function onChangeTimeOut() {
+      timeOutOptions.forEach(function (item, index) {
+        if (item.selected) {
+          timeInOptions[index].selected = true;
+        }
+      });
+    }
+    timeInSelect.addEventListener('change', onChangeTimeIn);
+    timeOutSelect.addEventListener('change', onChangeTimeOut);
+  }
+
   window.formValidation = {
-    roomsToGuestsValidation: roomsToGuestsValidation
+    roomsToGuestsValidation: roomsToGuestsValidation,
+    housngTypeToMinPriceValidation: housngTypeToMinPriceValidation,
+    getTimeSync: getTimeSync
   };
 })();
