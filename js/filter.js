5afebed73488deb8dc9267dbdd92871ec2b56824
Vladimir Legotin <wovanza@gmail.com>
2020-03-10 18:11:10 +0500

    Добавляет фильтры по цене, количеству гостей и комнат, выбранных фич

--- js/filter.js
+++ js/filter.js
@@ -5,6 +5,10 @@
   var MAX_PINS_QUANTITY = 5;
   var mapFilters = document.querySelector('.map__filters');
   var housingTypeSelector = document.querySelector('#housing-type');
+  var housingPriceSelector = document.querySelector('#housing-price');
+  var housingRoomsSelector = document.querySelector('#housing-rooms');
+  var housingGuestsSelector = document.querySelector('#housing-guests');
+  var housingFeatures = document.querySelectorAll('.map__checkbox');
   var selectFilterNodeList = mapFilters.querySelectorAll('select');
 
   function enableFilter(eventHandler) {
@@ -22,8 +26,48 @@
   }
 
   function filterByHousingType(offer) {
-    var filterValue = housingTypeSelector.value;
-    return (filterValue === 'any') || (filterValue === offer.offer.type);
+    var housingType = housingTypeSelector.value;
+    return (housingType === 'any') || (housingType === offer.offer.type);
+  }
+
+  function filterByHousingPrice(offer) {
+    var housingPrice = housingPriceSelector.value;
+    var result;
+
+    switch (housingPrice) {
+      case 'low':
+        result = offer.offer.price < 10000;
+        break;
+      case 'middle':
+        result = offer.offer.price >= 10000 && offer.offer.price <= 50000;
+        break;
+      case 'high':
+        result = offer.offer.price > 50000;
+        break;
+      case 'any':
+      default:
+        result = true;
+        break;
+    }
+    return result;
+  }
+
+  function filterByHousingRooms(offer) {
+    var housingRooms = housingRoomsSelector.value;
+    return housingRooms === 'any' ? true : offer.offer.rooms === Number(housingRooms);
+  }
+
+  function filterByHousingGuests(offer) {
+    var housingGuests = housingGuestsSelector.value;
+    return housingGuests === 'any' ? true : offer.offer.guests === Number(housingGuests);
+  }
+
+  function filterByHousingFeatures(offer) {
+    return Array.from(housingFeatures).filter(function (element) {
+      return element.checked;
+    }).every(function (feature) {
+      return offer.offer.features.includes(feature.value);
+    });
   }
 
   function returnFiltered(offers) {
@@ -32,18 +76,23 @@
 
     while (filteredOffers.length < MAX_PINS_QUANTITY && index < offers.length) {
       var offer = offers[index];
-      if (filterByHousingType(offer)) {
+
+      if (filterByHousingPrice(offer)
+       && filterByHousingType(offer)
+       && filterByHousingFeatures(offer)
+       && filterByHousingRooms(offer)
+       && filterByHousingGuests(offer)) {
         filteredOffers.push(offer);
       }
       index++;
     }
-
     return filteredOffers;
   }
 
   window.filter = {
     enable: enableFilter,
     disable: disableFilters,
-    returnFiltered: returnFiltered
+    returnFiltered: returnFiltered,
+    mapFilters: mapFilters
   };
 })();
